9704e7993691d9655977c0b91046bcdb62ce23bb
Eddie Sandler <ed.sandler@gmail.com>
2023-07-19 20:16:29 -0400

    code complete

--- app.py
+++ app.py
@@ -10,11 +10,28 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def show_form():
+    """function that takes in user inputs  and saves them as variables"""
 
     prompts = silly_story.prompts
 
     return render_template(
         "questions.html",
         prompts= prompts)
+
+@app.get('/results')
+def get_results_and_display():
+    """ function gets user inputs and renders story template"""
+    answers=request.args
+    result_story=silly_story.get_result_text(answers)
+    return render_template(
+        "results.html",
+        result_story=result_story
+
+    )
+
+
+
+
+
 
 
