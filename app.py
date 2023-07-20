c4f0d18930d1c82895bcf214bdddc2011f8d31ac
Eddie Sandler <ed.sandler@gmail.com>
2023-07-19 20:30:51 -0400

    done

--- app.py
+++ app.py
@@ -8,30 +8,29 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+
 @app.get('/')
 def show_form():
-    """function that takes in user inputs  and saves them as variables"""
+    """function  collects prompts from story instance and renders
+    them in a form inputs"""  #pass to templates /create form
 
     prompts = silly_story.prompts
 
-    return render_template(
+    return render_template (
         "questions.html",
-        prompts= prompts)
-
-@app.get('/results')
-def get_results_and_display():
-    """ function gets user inputs and renders story template"""
-    answers=request.args
-    result_story=silly_story.get_result_text(answers)
-    return render_template(
-        "results.html",
-        result_story=result_story
-
+        prompts=prompts
     )
 
 
+@app.get('/results')
+def get_results_and_display():
+    """ function gets form inputs and renders story result"""
+
+    answers = request.args
+    result_story = silly_story.get_result_text(answers)
 
 
-
-
-
+    return render_template(
+        "results.html",
+        result_story=result_story
+    )
